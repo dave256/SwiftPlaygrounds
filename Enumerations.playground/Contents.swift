a8a1d1565616e1f8f80043a828af88092a9f68ed
Dave Reed <dave256@mac.com>
2016-11-19 19:21:35 -0500

    add new Enumerations playground

---
+++ Enumerations.playground/Contents.swift
@@ -0,0 +1,86 @@
+//: ## enumerations are value types
+//: one of a finite set of options
+//: each option can also have an associated value
+
+//: ## basic enumerations
+enum Direction {
+    case north
+    case south
+    case west
+    case east
+}
+
+var direction: Direction = .north
+print(direction)
+
+
+//: ## enumeration with a type
+
+enum DayOfWeek : String {
+    case Sunday
+    case Monday
+    case Tuesday
+    case Wednesday
+    case Thursday
+    case Friday
+    case Saturday
+}
+
+var day : DayOfWeek = .Wednesday
+// vs. var day = "Sunday" which is susceptible to typo such as var day = "Wednsday"
+// var day: DayOfWeek = .Wednsday produces an error
+
+// prints actual value of the enumeration
+print(day)
+
+// since we gave the enum a type (String), we can use rawValue to get actual value
+let wed = day.rawValue
+print(wed)
+
+// can convert from String to enumerated value but it gives you an optional since string may not be a valid value
+day = DayOfWeek(rawValue: "Saturday")!
+day.rawValue
+
+//: ## enumeration with an associated value
+//:  can be useful for return value from function that might fail
+
+//:  can return Ok or an Error with an associated value
+enum MyError {
+    case Ok
+    case Error(String)
+}
+
+var e1: MyError = .Ok
+var e2: MyError = .Error("An error message")
+
+switch e1 {
+case .Ok:
+    print("no error")
+case .Error(let msg):
+    print("error: \(msg)")
+}
+switch e2 {
+case .Ok:
+    print("no error")
+case .Error(let msg):
+    print("error: \(msg)")
+}
+
+// show that Enumerations are value types
+var e3 = e2
+e2 = .Ok
+switch e2 {
+case .Ok:
+    print("no error")
+case .Error(let msg):
+    print("error: \(msg)")
+}
+
+// e3 is still the one with error message
+switch e3 {
+case .Ok:
+    print("no error")
+case .Error(let msg):
+    print("error: \(msg)")
+}
+
